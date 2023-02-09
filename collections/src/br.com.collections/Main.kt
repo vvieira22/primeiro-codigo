60d6a0fc616112feff487b812096f9f54821f8db
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2023-02-09 08:24:06 -0300

    exemplos de utilização map (dicionario) no kotlin.

--- collections/src/br.com.collections/Main.kt
+++ collections/src/br.com.collections/Main.kt
@@ -1,5 +1,43 @@
 fun main() {
-    testaSetCollections()
+    val usuarios = mapOf<String, Long>(
+    "Maicon" to 52256334237,
+    "Vitor" to 32256331237,
+    "Mariana" to 92256993237)
+
+    println(usuarios)
+
+    for(par in usuarios)
+    {
+        print("${par.key}: ")
+        println(par.value)
+    }
+
+    //Tentando achar um valor para uma chave
+    usuarios[""]?.let{
+        println("Achou a pessoa: $it")
+    }
+
+    //Comportamento de escrita
+    val usuarios2 : MutableMap<String, Long> = mutableMapOf(
+        "Maicon" to 52256334237,
+        "Vitor" to 32256331237,
+        "Mariana" to 92256993237)
+
+    usuarios2["Joao"] = 922569522563
+    //ou
+    usuarios2.put("Joao", 922569522563)
+
+    //Adicionar se ele realmente nao existir
+    usuarios2.putIfAbsent("Joao", 922569522563)
+
+    //Remocao
+    usuarios2.remove("Joao")
+    //ou
+    //aqui gera sobrecarga, ambos precisam estar certo pra remover.
+    usuarios2.remove("Joao", 922569522563)
+
+    println(usuarios2)
+
 }
 
 
