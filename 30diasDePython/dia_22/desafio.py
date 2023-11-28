854ce5ee37935e561b890cfcb423a4e31969420d
Vitor Vieira <vitorgoncalvesvieira22@gmail.com>
2023-11-28 07:52:27 -0300

    desafio 2 adicionado, dia 22, 30 dias de python.

--- 30diasDePython/dia_22/desafio.py
+++ 30diasDePython/dia_22/desafio.py
@@ -20,7 +20,28 @@ list_items = soup.find_all('li', class_='list-item')
 data = {item.find('p', class_='text').get_text(): item.find('span', class_='value').get_text() for item in list_items}
 
 # Convert the dictionary to JSON
-json_data = json.dumps(data)
+json_data = json.dumps(data, indent=4)
 
 # Print the JSON data
+# print(json_data)
+
+
+#2
+
+# URL to fetch
+url = "https://archive.ics.uci.edu/datasets"
+
+# Fetch the content from url
+response = requests.get(url)
+content = response.content
+
+# Parse the content with BeautifulSoup
+soup = BeautifulSoup(content, 'html.parser')
+soup = soup.find('div', class_='flex flex-col gap-1')
+soup = soup.find_all('div', class_='rounded-box bg-base-100')   
+
+# Find all li elements with class 'link-hover link text-xl font-semibold'
+# name = soup.find_all('a', class_='link-hover link text-xl font-semibold')
+data = {item.find('a', class_='link-hover link text-xl font-semibold').get_text(): item.find('p', class_='truncate').get_text() for item in soup}
+json_data = json.dumps(data, indent=3)
 print(json_data)
