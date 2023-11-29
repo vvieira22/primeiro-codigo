494f7f632055519fbde6102cca0838e7ac93eea3
Vitor Vieira <vitor.vieira@conductor.com.br>
2023-11-29 12:03:43 -0300

    dia 24 adicionado, (30 dias de python).

---
+++ 30diasDePython/dia_24/estatisticas.py
@@ -0,0 +1,28 @@
+"""
+ESTATÍSTICAS
+
+Estatística é a disciplina que estuda a coleta , organização , exibição , análise , interpretação e apresentação de dados. 
+A estatística é um ramo da matemática recomendado como um pré-requisito para a ciência de dados e o aprendizado de máquina.
+
+
+DADOS
+O que são dados? Dados são qualquer conjunto de caracteres coletados e traduzidos para algum propósito, geralmente análise. 
+Pode ser qualquer caractere, incluindo texto e números, imagens, som ou vídeo. Se os dados não forem colocados em um contexto,
+não farão sentido para um ser humano ou para um computador. Para entender os dados, precisamos trabalhar nos dados usando ferramentas diferentes.
+
+ 
+
+Módulo de Estatísticas
+O módulo de estatísticas Python fornece funções para calcular estatísticas matemáticas de dados numéricos. O módulo não se destina a ser um 
+concorrente de bibliotecas de terceiros, como NumPy, SciPy, ou pacotes estatísticos proprietários completos 
+destinados a estatísticos profissionais, como Minitab, SAS e Matlab. Destina-se ao nível de calculadoras gráficas e científicas.
+
+
+NumPy
+
+NumPy é a biblioteca principal para computação científica em Python. 
+Ele fornece um objeto array multidimensional de alto desempenho e ferramentas para trabalhar com arrays.
+
+Vamos utilizar o jupyter notebook para melhor visualizar, com o arquivo estatisticas.ipynb
+
+"""
