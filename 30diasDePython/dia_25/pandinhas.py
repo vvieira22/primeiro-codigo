2b309f9d6a85272fbdb297039fd457675836396c
Vitor Vieira <vitor.vieira@conductor.com.br>
2023-11-29 15:44:35 -0300

    dia 24 adicionado, (30 dias de python.)

---
+++ 30diasDePython/dia_25/pandinhas.py
@@ -0,0 +1,14 @@
+"""
+Pandas é uma estrutura de dados e ferramentas de análise de dados de código aberto, de alto desempenho e 
+fácil de usar para a linguagem de programação Python. Pandas adiciona estruturas de dados e ferramentas projetadas para 
+trabalhar com dados semelhantes a tabelas, que são Series e Data Frames . Pandas fornece ferramentas para manipulação de dados:
+
+    remodelando
+    fundindo
+    Ordenação
+    fatiamento
+    agregação
+    imputação. Se você estiver usando o anaconda, não será necessário instalar o pandas.
+"""
+
+#Da mesma forma que foi trabalhado o numpy o pandas sera utilizada em um notebook (jupyter).
