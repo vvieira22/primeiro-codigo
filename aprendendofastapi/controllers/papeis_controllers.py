5ac9e4ef64a865abb8b2d04338c050091120d06c
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2024-01-14 09:46:29 -0300

    obtendo papel por id, junto com testes.

--- aprendendofastapi/controllers/papeis_controllers.py
+++ aprendendofastapi/controllers/papeis_controllers.py
@@ -1,6 +1,7 @@
 from models.papel import Papel
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Response
 import json
+import ormar 
 
 router =  APIRouter()
 
@@ -12,3 +13,11 @@ async def adicionar_papel(papel: Papel):
 @router.get("/")
 async def listar_papel():
     return await Papel.objects.all()
+
+@router.get("/{id}")
+async def pegar_papel(id: int, response: Response):
+    try:
+        return await Papel.objects.get(id=id)
+    except ormar.exceptions.NoMatch:   #nao e bom botar qualquer execao pois pode dar 500, ou outro erro, importante mapear.
+        response.status_code = 404
+        return {"mensagem": "Entidade nao encontrada."}
