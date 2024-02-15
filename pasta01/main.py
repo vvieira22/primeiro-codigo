ad71f6371339d878b96a99b3d7d6a9777046980e
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2024-02-15 18:54:18 -0300

    add.

--- pasta01/main.py
+++ pasta01/main.py
@@ -2,18 +2,13 @@ import subprocess
 import os
 import git
 
-print("100")
-print("200")
-
-directory = os.getcwd().replace('\\', '/')
-print("300")
-
 # Abra o repositório
 repo = git.Repo("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\teste")
-print("400")
 repo.git.fetch("-p", "--tags")
-print("500")
 
 # Obtenha as três últimas tags ordenadas por data de criação
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime, reverse=True)[:3]
-print("600")
+
+# Imprima as tags
+for tag in tags:
+    print(tag.name)
