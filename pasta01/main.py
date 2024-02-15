8b536eaa5ab5c8ad3aca714990c37425fb4a86b9
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2024-02-15 18:31:01 -0300

    edit.

--- pasta01/main.py
+++ pasta01/main.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 import git
-import os
 
 directory = os.getcwd().replace('\\', '/')
 
@@ -9,7 +8,7 @@ directory = os.getcwd().replace('\\', '/')
 repo = git.Repo(directory)
 
 # Obtenha as três últimas tags ordenadas por data de criação
-tags = repo.tags.sort(key=lambda t: t.commit.committed_datetime, reverse=True)[:3]
+tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime, reverse=True)[:3]
 
 # Imprima as tags
 for tag in tags:
