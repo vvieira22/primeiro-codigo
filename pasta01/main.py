b334287337be142d95d592ad3e56c8073856d676
Vitor Gonçalves Vieira <49120447+vvieira22@users.noreply.github.com>
2024-02-15 12:44:29 -0300

    Update main.py

--- pasta01/main.py
+++ pasta01/main.py
@@ -1,6 +1,15 @@
-import json
+import subprocess
+import os
 
-# a Python object (dict):
-lista = ["rs","xd"]
-# print the result to transfer to caller:
-print("rs")
+cwd = os.getcwd()
+# Defina o comando como uma string
+command = "git tag --sort=-creatordate | head -n 3"
+
+# Execute o comando 
+process = subprocess.Popen(command, cwd = cwd, stdout=subprocess.PIPE, shell=True, text=True)
+
+# Obtenha a saída do comando
+output, error = process.communicate()
+
+# Imprima a saída
+print(output)
