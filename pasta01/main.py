abc36a4668c610eb09d9d9b4190c292973d7c73e
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2024-02-15 18:39:59 -0300

    edit.

--- pasta01/main.py
+++ pasta01/main.py
@@ -10,6 +10,4 @@ repo = git.Repo(directory)
 # Obtenha as três últimas tags ordenadas por data de criação
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime, reverse=True)[:3]
 
-# Imprima as tags
-for tag in tags:
-    print(tag.name)
+print(tags)
