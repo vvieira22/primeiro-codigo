2c2cd05e8bf783d4bd7ac872d9b4eb0f2cce4cef
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2024-02-15 19:00:59 -0300

    .

--- pasta01/main.py
+++ pasta01/main.py
@@ -4,7 +4,8 @@ import git
 
 # Abra o repositório
 repo = git.Repo("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\teste")
-repo.git.fetch("-p", "--tags")
+for remote in repo.remotes:
+    remote.fetch()
 
 # Obtenha as três últimas tags ordenadas por data de criação
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime, reverse=True)[:3]
