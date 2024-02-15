1e303886e70aabc06b2b85054430c14abc24683b
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2024-02-15 18:44:51 -0300

    edit.

--- pasta01/main.py
+++ pasta01/main.py
@@ -2,12 +2,14 @@ import subprocess
 import os
 import git
 
-print("100")
+print("100","200")
 
 directory = os.getcwd().replace('\\', '/')
 
 # Abra o repositório
 repo = git.Repo(directory)
+fetch = repo.git.fetch("-p", "--tags")
+print(fetch)
 
 # Obtenha as três últimas tags ordenadas por data de criação
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime, reverse=True)[:3]
