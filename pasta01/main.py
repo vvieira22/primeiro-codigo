0908b2ccd05040c4b338381112d837fa1bc67bed
Vitor Gonçalves Vieira <49120447+vvieira22@users.noreply.github.com>
2024-02-15 12:06:28 -0300

    Update main.py

--- pasta01/main.py
+++ pasta01/main.py
@@ -1,14 +1,6 @@
 import json
 
 # a Python object (dict):
-x = {
-  "name": "John",
-  "age": 30,
-  "city": "New York"
-}
-
-# convert into JSON:
-y = json.dumps(x)
-
+lista = ["rs","xd"]
 # print the result to transfer to caller:
-print(y)
+print(lista)
