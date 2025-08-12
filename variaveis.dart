33a6524537d66053fa104262a8d04d6eb51c73bb
Vitor Vieira <vitorgoncalvesvieira22@gmail.com>
2025-08-12 20:03:33 -0300

    primeiro commit dart.

---
+++ variaveis.dart
@@ -0,0 +1,7 @@
+void main() {
+  int idade = 25;
+  String nome = 'Vitor';
+  double altura = 1.80;
+  bool estudante = true;
+  print('Nome: $nome, Idade: $idade, Altura: $altura, Estudante: $estudante');
+}
