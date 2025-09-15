24571e530e8b2536c1b412dc313b90f65670f4d8
Vitor Vieira <vitorgoncalvesvieira22@gmail.com>
2025-09-15 11:50:16 -0300

    renomeando projeto e alterando pastas.

--- silk_deaths/android/app/build.gradle.kts
+++ silk_deaths/android/app/build.gradle.kts
@@ -13,7 +13,7 @@ if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 android {
-    namespace = "com.example.contador_pessoas"
+    namespace = "com.example.silk_deaths"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.vvieira.silkdeath"
+        applicationId = "com.vvieira.silkdeaths"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
