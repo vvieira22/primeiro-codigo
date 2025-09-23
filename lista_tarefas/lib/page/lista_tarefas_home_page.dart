dde270e62eaf2c48b7094325277ad34ce72f639e
Vitor Vieira <vitorgoncalvesvieira22@gmail.com>
2025-09-22 23:50:49 -0300

    melhoras adicionando e removendo tarefas da lista.

--- lista_tarefas/lib/page/lista_tarefas_home_page.dart
+++ lista_tarefas/lib/page/lista_tarefas_home_page.dart
@@ -4,7 +4,7 @@ import 'package:lista_tarefas/widgets/lista_tarefas_item.dart';
 import '../models/tarefa.dart';
 
 class TodoListHomePage extends StatefulWidget {
-  TodoListHomePage({super.key});
+  const TodoListHomePage({super.key});
 
   @override
   State<TodoListHomePage> createState() => _TodoListHomePageState();
@@ -12,15 +12,18 @@ class TodoListHomePage extends StatefulWidget {
 
 class _TodoListHomePageState extends State<TodoListHomePage> {
   final TextEditingController campoTarefa = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   List<Tarefa> tarefas = [];
+  Tarefa? tarefaDeletada;
+  int? indiceTarefaDeletada;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(24,122,24,122),
+          padding: const EdgeInsets.fromLTRB(24, 122, 24, 122),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             //ISSO DIZ QUE A COLUNA VAI TER SO O TAMANHO DOS ELEMENTOS!
@@ -31,6 +34,7 @@ class _TodoListHomePageState extends State<TodoListHomePage> {
                   Expanded(
                     // flex:2, //valores relativos de acordo com Expanded da regiao.
                     child: TextField(
+                      focusNode: _focusNode,
                       controller: campoTarefa,
                       decoration: InputDecoration(
                         hintText: 'Ex. Estudar Python.',
@@ -64,22 +68,25 @@ class _TodoListHomePageState extends State<TodoListHomePage> {
                 ],
               ),
               SizedBox(height: 16),
-              Flexible( //Flexible limita os componentes ao maximo possivel sem quebrar layout. e força scroll junto com o shrinkWrap.
+              Flexible(
+                //Flexible limita os componentes ao maximo possivel sem quebrar layout. e força scroll junto com o shrinkWrap.
                 child: ListView(
                   shrinkWrap: true,
                   //aumenta tamanho baseado no espaço disponivel.
                   children: [
-                    for(Tarefa tarefa in tarefas)
-                      ListaTarefasItem(
-                          tarefa: tarefa
-                      ),
+                    for (Tarefa tarefa in tarefas)
+                      ListaTarefasItem(tarefa: tarefa, onDelete: onDelete),
                   ],
                 ),
               ),
               SizedBox(height: 16),
               Row(
                 children: [
-                  Expanded(child: Text('Você possui ${tarefas.length} tarefas pendentes.')),
+                  Expanded(
+                    child: Text(
+                      'Você possui ${tarefas.length} tarefas pendentes.',
+                    ),
+                  ),
                   ElevatedButton(
                     onPressed: null,
                     style: ElevatedButton.styleFrom(
@@ -107,6 +114,8 @@ class _TodoListHomePageState extends State<TodoListHomePage> {
       data: DateTime.now(),
     );
     setState(() {
+      campoTarefa.clear();
+      _focusNode.requestFocus();
       tarefas.add(novaTarefa);
     });
   }
@@ -118,5 +127,51 @@ class _TodoListHomePageState extends State<TodoListHomePage> {
   //Sempre que eu digitar texto e apertar "enter/ok do android ".
   void onSubmitted(String text) {
     print(text);
+  }
+
+  void onDelete(Tarefa todo) {
+    tarefaDeletada = todo;
+    indiceTarefaDeletada = tarefas.indexOf(todo);
+
+    setState(() {
+      tarefas.remove(todo);
+    });
+
+    // Para Snackbar com botao nao tem timeout nem duracao, tem que fazer
+    // essa maldita gambiarra
+    Future.delayed(const Duration(seconds: 3), () {
+      ScaffoldMessenger.of(context).clearSnackBars();
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: RichText(
+          text: TextSpan(
+            text: 'Tarefa ',
+            children: <TextSpan>[
+              TextSpan(text: todo.nomeTarefa, style: TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: ' removida!'),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.red,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        behavior: SnackBarBehavior.floating, // Para que o shape funcione
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8), // Margem para melhor visualização
+        padding: EdgeInsets.all(16), // Padding interno
+        action: SnackBarAction(
+          label: 'Desfazer'.toUpperCase(),
+          textColor: Colors.lightBlue,
+          onPressed: () {
+            setState(() {
+              tarefas.insert(indiceTarefaDeletada!, tarefaDeletada as Tarefa);
+            });
+          },
+        ),
+      ),
+    );
   }
 }
--- lista_tarefas/lib/widgets/lista_tarefas_item.dart
+++ lista_tarefas/lib/widgets/lista_tarefas_item.dart
@@ -1,38 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
+
 import '../models/tarefa.dart';
 
 //Sempre começa com stateless e ver se precisa mudar para stateful.
 class ListaTarefasItem extends StatelessWidget {
-  const ListaTarefasItem({super.key, required this.tarefa});
+  const ListaTarefasItem({super.key, required this.tarefa, required this.onDelete});
 
   final Tarefa tarefa;
+  final Function(Tarefa) onDelete;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[300],
-          borderRadius: BorderRadius.circular(5),
-        ),
-        padding: EdgeInsets.all(8),
-        //Container ja tem padding nao precisa colocar widget nele.
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const DrawerMotion(),
+          extentRatio: 0.5,
           children: [
-            Text(
-              DateFormat('dd/MM/yyyy - HH:mm:ss').format(tarefa.data).toString(),
+            SlidableAction(
+              label: 'Editar',
+              backgroundColor: Colors.blue,
+              icon: Icons.edit,
+              onPressed: (context) {},
             ),
-            Text(
-              tarefa.nomeTarefa,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ), //Style do texto
+            SlidableAction(
+              label: 'Excluir',
+              backgroundColor: Colors.red,
+              icon: Icons.delete,
+              onPressed: (context) {
+                onDelete(tarefa);
+              },
             ),
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: EdgeInsets.all(16),
+            //Container ja tem padding nao precisa colocar widget nele.
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  DateFormat(
+                    'dd/MM/yyyy - HH:mm:ss',
+                  ).format(tarefa.data).toString(),
+                ),
+                Text(
+                  tarefa.nomeTarefa,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ), //Style do texto
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
