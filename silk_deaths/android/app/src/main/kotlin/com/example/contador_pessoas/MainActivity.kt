027155f57c62e1a7872d737d9e131afa83aad04d
Vitor vieira <vitorgoncalvesvieira22@gmail.com>
2026-04-11 22:15:53 -0300

    migrate repository to vvieira22/silk_deaths.

--- silk_deaths/android/app/src/main/kotlin/com/example/contador_pessoas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.silk_deaths
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- silk_deaths/lib/extensions/int_extensions.dart
+++
@@ -1,4 +0,0 @@
-extension IntExtensions on int {
-  bool canDecrease() =>
-    this > 0 ? true : false;
-}
